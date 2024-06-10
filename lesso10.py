db70f067cb8365d114765814aa0449e866571faf
romankostuk <moryak.man@list.ru>
2024-06-10 22:52:09 +0300

    Add files via upload

---
+++ lesso10.py
@@ -0,0 +1,7 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+while True:
+    number = int(input('Введите число из списка: '))
+    if number >= 0:
+        print('Отличный результат')
+    else:
+        break
